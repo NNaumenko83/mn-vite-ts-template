54de68404f311fa762a192c296939f4e4d08f9ce
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-12-26 13:18:08 +0200

    ADD: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,17 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    "airbnb",
+    'airbnb-typescript',
+    "airbnb/hooks",
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+  project: './tsconfig.json'
+},
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
